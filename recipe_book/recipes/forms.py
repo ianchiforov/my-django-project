0908dc27d84a98fbd308d194543b0c30e4acedb5
ianchiforov <ianchiforov@gmail.com>
2022-10-23 22:42:35 +0300

    finish RecipeForm

--- recipe_book/recipes/forms.py
+++ recipe_book/recipes/forms.py
@@ -1,5 +1,5 @@
 from .models import Recipe
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 
 class RecipeForm(ModelForm):
@@ -7,5 +7,19 @@ class RecipeForm(ModelForm):
         model = Recipe
         fields = "__all__"
         widgets = {
-            ""
+            "time_to_cook": TextInput(attrs={
+                "type": "number",
+                "min": 1,
+                "max": 999
+            }),
+            "complexity": TextInput(attrs={
+                "type": "number",
+                "min": 1,
+                "max": 5
+            }),
+            "calories": TextInput(attrs={
+                "type": "number",
+                "min": 1,
+                "max": 9999
+            })
         }
