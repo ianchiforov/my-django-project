67b91c6fc5cd6c541a96ab54b11e0f1d7d8b5a0f
ianchiforov <ianchiforov@gmail.com>
2022-10-23 22:41:02 +0300

    show all recipes on the welcome page

--- recipe_book/main_website/views.py
+++ recipe_book/main_website/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from recipes.models import Recipe
 
 
 # Create your views here.
 def welcome(request):
-    return render(request, "main_website/welcome.html")
+    return render(request, "main_website/welcome.html",
+                  {
+                      "recipes": Recipe.objects.all()
+                  })
