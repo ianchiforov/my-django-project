4f5ce18762c73bdbfc174bd92d6a529770b9e4b4
ianchiforov <ianchiforov@gmail.com>
2022-10-23 22:41:57 +0300

    include recipes urls

--- recipe_book/recipe_book/urls.py
+++ recipe_book/recipe_book/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from main_website.views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome)
+    path('', welcome, name='welcome'),
+    path('recipes/', include('recipes.urls'))
 ]
