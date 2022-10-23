7a50b131d5389c134453f06959307656cd70fb99
ianchiforov <ianchiforov@gmail.com>
2022-10-23 22:42:56 +0300

    finish Recipe model

--- recipe_book/recipes/models.py
+++ recipe_book/recipes/models.py
@@ -3,17 +3,29 @@ from django.utils.text import slugify
 from django.db.models import signals
 from django.dispatch import receiver
 from datetime import time
+from autoslug import AutoSlugField
 
 
 # Create your models here.
-class Recipe:
+class Recipe(models.Model):
     name = models.CharField(max_length=120)
-    time_to_cook = models.TimeField(default=time(1))
+    time_to_cook = models.IntegerField()
     complexity = models.IntegerField()
     calories = models.IntegerField()
     recipe_text = models.TextField()
+    slug = AutoSlugField(populate_from='name',
+                         primary_key=True,
+                         unique=True,
+                         )
+
+    def __str__(self):
+        return f"""{self.name} ({self.slug})
+        time to cook: {self.time_to_cook} minutes
+        complexity: {self.complexity} stars (out of 5)
+        calories: {self.calories}
+        recipe text: {self.recipe_text}"""
 
 
-@receiver(signals.pre_save, sender=Recipe)
-def populate_slug(sender, instance, **kwargs):
-    instance.slug = slugify(instance.name)
+# @receiver(signals.pre_save, sender=Recipe)
+# def populate_slug(sender, instance, **kwargs):
+#     instance.slug = slugify(instance.name)
