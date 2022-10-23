12e81e573c255ae49294f82138818a26d4798c77
ianchiforov <ianchiforov@gmail.com>
2022-10-23 22:43:29 +0300

    implement detail and new view

--- recipe_book/recipes/views.py
+++ recipe_book/recipes/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Recipe
+from .forms import RecipeForm
+
 
 # Create your views here.
+def detail(request, slug):
+    recipe = get_object_or_404(Recipe, pk=slug)
+    return render(request, "recipes/detail.html",
+                  {
+                      "recipe": recipe
+                  })
+
+
+def new(request):
+    if request.method == "POST":
+        form = RecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("welcome")
+    else:
+        form = RecipeForm()
+    return render(request, "recipes/new.html",
+                  {
+                      "form": form
+                  })
